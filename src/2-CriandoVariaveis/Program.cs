8e046d5ba9cbd944d04849aef4f4b989f7a97178
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-03-19 16:56:53 -0300

    Video 3.1

---
+++ src/2-CriandoVariaveis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 2-CriandoVariaveis em execução...");
+
+            int anoCriacaoCSharp;
+            anoCriacaoCSharp = 2002;
+
+            Console.WriteLine(anoCriacaoCSharp);
+
+            Console.WriteLine("Finalizada a execução. Dê enter para encerrar o processo...");
+            Console.ReadLine();
+        }
+    }
+}
