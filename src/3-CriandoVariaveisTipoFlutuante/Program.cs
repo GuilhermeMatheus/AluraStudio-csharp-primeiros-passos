7ad476cf8a6d8ba5c111d732d9ed9d6660968903
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-03-21 14:40:44 -0300

    Video 32 - regravado

---
+++ src/3-CriandoVariaveisTipoFlutuante/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_CriandoVariaveisTipoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 3 CriandoVariaveisTipoFlutuante");
+
+            double salario;
+            salario = 1200.50;
+
+            Console.WriteLine(salario);
+
+            double idade;
+            idade = 31.0 / 2;
+            Console.WriteLine(idade);
+
+            double novaIdade;
+            novaIdade = 5.0 / 3;
+            Console.WriteLine(novaIdade);
+
+            int outraIdade = 5;
+            Console.Write(outraIdade);
+
+            Console.WriteLine("A aplicação finalizou. Tecle enter para sair . . . ");
+            Console.ReadLine();
+        }
+    }
+}
