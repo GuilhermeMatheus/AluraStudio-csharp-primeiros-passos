ccb565d237db8f94e8cd54ec49e8d57892e5d8c1
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-03-22 16:25:42 -0300

    Video 5.2

--- src/8-ContinuacaoCondicionais/Program.cs
+++ src/8-ContinuacaoCondicionais/Program.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _7_Condicionais
+namespace _8_ContinuacaoCondicionais
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Executando projeto 7 - Condicionais");
+            Console.WriteLine("Executando projeto 8 - Continuação Condicionais");
 
             int idadeJoao = 16;
             int quantidadePessoas = 2;
@@ -19,17 +19,15 @@ namespace _7_Condicionais
             {
                 Console.WriteLine("João possui mais de 18 anos de idade. Pode entrar.");
             }
+            else if (quantidadePessoas >= 2)
+            {
+                Console.WriteLine("João não possui mais de 18 anos, mas está acompanhado. Pode entrar");
+            } 
             else
             {
-                if (quantidadePessoas >= 2)
-                {
-                    Console.WriteLine("João não possui mais de 18 anos, mas está acompanhado. Pode entrar");
-                }
-                else
-                {
-                    Console.WriteLine("João não possui mais de 18 anos, não pode entrar");
-                }
+                Console.WriteLine("João não possui mais de 18 anos, não pode entrar");
             }
+
 
             Console.ReadLine();
         }
